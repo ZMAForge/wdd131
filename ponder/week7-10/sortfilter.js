7a85319415535cf3403e21c651a8cc60697c5bb3
ZMAForge <zeldamarieasvitt@outlook.com>
2026-06-24 13:34:07 -0700

    ponder complete

---
+++ ponder/week7-10/sortfilter.js
@@ -0,0 +1,96 @@
+nums = [12, 10, 8, 3];
+
+console.log(nums.sort(compareFn));
+
+
+function compareFn(a,b) {
+  if (a.price < b.price) {
+    return -1;
+  } else if (a.price > b.price) {
+    return 1;
+  }
+ return 0;
+}
+
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+
+let simpleSort = simpleList.sort();
+console.log(simpleSort)
+
+let lowerlist = simpleList.map(function(fruit) {
+    return fruit.toLowerCase();
+})
+
+let lowerSort = lowerlist.sort();
+console.log(lowerSort);
+
+let searchTerm = 'an';
+let filterFruit = lowerSort.filter(searchFruit);
+
+function searchFruit(item) {
+    return item.includes(searchTerm);
+}
+
+console.log(filterFruit);
+
+
+
+const products = [
+  {
+    productName: "Wireless Mouse",
+    price: 29.99
+  },
+  {
+    productName: "Bluetooth Keyboard",
+    price: 49.99
+  },
+  {
+    productName: "Laptop Stand",
+    price: 39.99
+  }
+];
+
+let productSort = products.sort(compareFn);
+
+console.log(productSort);
+
+
+const animals = [
+  {
+    name: "Lion",
+    traits: ["brave", "strong", "fierce", "wild"]
+  },
+  {
+    name: "Elephant",
+    traits: ["large", "gentle", "smart", "wild"]
+  },
+  {
+    name: "Fox",
+    traits: ["sly", "quick", "clever", "wild"]
+  },
+  {
+    name: "Dog",
+    traits: ["loyal", "friendly", "playful", "cuddly"]
+  },
+  {
+    name: "Cat",
+    traits: ["quiet", "independent", "curious", "cuddly"]
+  }
+];
+
+let queary = 'dog';
+let filteredList = animals.filter(searchList);
+function searchList (item) {
+    return item.name.toLowerCase().includes(queary.toLowerCase());
+}
+
+console.log(filteredList);
+
+let quearyTrait = 'cuddly';
+let filteredTraits = animals.filter(searchTraits);
+function searchTraits (item) {
+    return item.traits.find((trait) => trait.toLowerCase().includes(quearyTrait.toLowerCase()));
+}
+
+console.log(filteredTraits);    
